89612ec3675d5789d06fd8b6e1e524f930729266
ayuub <yub2tech@gmail.com>
2023-01-29 18:04:53 +0000

    Initial Commit

--- server.py
+++ server.py
@@ -1,33 +1,52 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import pandas
 import random
 
 app = Flask(__name__)
 
 
-df = pandas.read_csv("london_temp.csv")
-random_entry = random.randint(0, 389976)
-random_year_info = df.loc[random_entry]
-random_year_time = random_year_info["dt_iso"]
-random_year_temp = random_year_info["temp"]
-random_year = random_year_time[0:11]
-random_time = random_year_time[11:20]
 
-@app.route('/')
+# df = pandas.read_csv("london_temp.csv")
+# random_entry = random.randint(0, 389976)
+# random_entry = random.randint(0, 389976)
+# random_year_info = df.loc[random_entry]
+# random_year_time = random_year_info["dt_iso"]
+# random_year_temp = random_year_info["temp"]
+# random_year = random_year_time[0:11]
+# random_time = random_year_time[11:20]
+
+@app.route('/', methods=['POST', 'GET'])
 def home():
+    df = pandas.read_csv("london_temp.csv")
+    random_entry = random.randint(0, 389976)
+    random_year_info = df.loc[random_entry]
+    random_year_time = random_year_info["dt_iso"]
+    random_year_temp = random_year_info["temp"]
+    random_year = random_year_time[0:11]
+    random_time = random_year_time[11:20]
+    if request.method == 'POST':
+        guess = request.form['user_guess']
+        type_guess = round(int(guess))
+        if type_guess == round(random_year_temp):
+            return render_template("results.html", guess=guess, random_year_time_temp=round(random_year_temp))
+            print(f"The day answered is {random_year_time} and the temp is {random_year_temp}")
+        else:
+            return render_template("incorrect.html", guess=guess, random_year_time_temp=round(random_year_temp))
+            print(f"The day answered is {random_year_time} and the temp is {random_year_temp}")
+
     return render_template("index.html", random_year=random_year, random_time=random_time)
 
-@app.route('/', methods=['POST'])
-def guess_temp():
-    guess = request.form['user_guess']
-    type_guess = round(int(guess))
-    if request.method == "POST":
-        with open("guesses.txt", "a") as g:
-            g.write(f"User guessed: {guess}, the actual temperature was {round(random_year_temp)}\n")
-    if type_guess == round(random_year_temp):
-        return render_template("results.html", guess=guess, random_year_time_temp=round(random_year_temp))
-    else:
-        return render_template("incorrect.html", guess=guess, random_year_time_temp=round(random_year_temp))
+# @app.route('/', methods=['POST'])
+# def guess_temp():
+#     if request.method == "POST":
+#         guess = request.form['user_guess']
+#         type_guess = round(int(guess))
+#         with open("guesses.txt", "a") as g:
+#             g.write(f"User guessed: {guess}, the actual temperature was {round(random_year_temp)}\n")
+#         if type_guess == round(random_year_temp):
+#             return render_template("results.html", guess=guess, random_year_time_temp=round(random_year_temp))
+#         else:
+#             return render_template("incorrect.html", guess=guess, random_year_time_temp=round(random_year_temp))
 
 
 if __name__ == "__main__":
